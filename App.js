9d9e5a6d83cf0b4a45e83e35d3936a09b12bb925
amal-nj <amal.alnajjar96@gmail.com>
2020-04-26 15:24:41 +0300

    basic map done

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 // import { createStackNavigator } from '@react-navigation/stack';
@@ -10,7 +10,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { createAppContainer, createSwitchNavigator, StackNavigator } from "react-navigation";
 import FetchData from './Components/FetchData.js';
 import FirstScreen from './Components/FirstScreen.js';
-
+import axios from 'axios'
 
 //Stack navigation
 const Stack = createStackNavigator();
@@ -29,7 +29,7 @@ class HomeStack extends React.Component {
 
   render() {
     return (
-      <MyStack/>
+      <MyStack />
     );
   }
 
@@ -51,10 +51,60 @@ const AppContainer = createAppContainer(
 );
 
 export default function App() {
+
+
+
+  // useEffect(() => {
+  //   console.log("function called")
+  //   const latitude = 21.6035169; // you can update it with user's latitude & Longitude
+  //   const longitude = 39.2526234;
+  //   let radMetter = 2 * 1000; // Search withing 2 KM radius
+
+  //   const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + latitude + ',' + longitude + '&radius=' + radMetter +'&language=en'+'&key=' + 'AIzaSyA8T_Kl0AIGpKN-9vqEPbZodiZDktG1KjY'
+ 
+  //   fetch(url)
+  //    .then(resp => resp.json())
+  //     .then(res => {
+  //       // console.log(res)
+  //       var places = [] // This Array WIll contain locations received from google
+  //       for (let googlePlace of res.results) {
+  //         var place = {}
+  //         var lat = googlePlace.geometry.location.lat;
+  //         var lng = googlePlace.geometry.location.lng;
+  //         var coordinate = {
+  //           latitude: lat,
+  //           longitude: lng,
+  //         }
+
+  //         var gallery = []
+ 
+
+  //         place['placeTypes'] = googlePlace.types
+  //         place['coordinate'] = coordinate
+  //         place['placeId'] = googlePlace.place_id
+  //         place['placeName'] = googlePlace.name
+  //         place['gallery'] = gallery
+
+  //         places.push(place);
+  //       }
+  //       console.log('places:')
+  //       console.log(places)
+  //       console.log(places.length)
+  //       console.log(places[1].placeName)
+  //       global.placeName=places[1].placeName
+  //       //Do your work here with places Array
+  //     })
+  //     .catch(error => {
+  //       console.log("error:")
+  //       console.log(error);
+  //     });
+
+
+  // }, [])
   return (
     <NavigationContainer>
-      <AppContainer/>
-  
+      <AppContainer />
+
     </NavigationContainer>
   );
 }
--- Components/HomeScreen.js
+++ Components/HomeScreen.js
@@ -1,21 +1,121 @@
-import * as React from 'react';
-import { Button, View, Text, Dimensions } from 'react-native';
-// 
+import React, { useState, useEffect } from 'react';
+import MapView from 'react-native-maps';
+import { Button, Platform, StyleSheet, View, Text, Dimensions, processColor } from 'react-native';
+// import React, { useState, useEffect } from 'react';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+
+
 export default function HomeScreen({ navigation }) {
+  const [location, setLocation] = useState( {"timestamp":1587559966314,"mocked":false,"coords":{"altitude":0,"heading":313.141845703125,"longitude":0,"speed":0.36433911323547363,"latitude":0,"accuracy":25.93199920654297}});
+  const [errorMsg, setErrorMsg] = useState(null);
 
-  return (
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+      }
 
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+
+
+      console.log("function called")
+      const latitude = location.coords.latitude; // you can update it with user's latitude & Longitude
+      const longitude = location.coords.longitude;
+      let radMetter = 0.4* 1000; // Search withing 2 KM radius
+      console.log('key:'+process.env.API_KEY)
+      const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + latitude + ',' + longitude + '&radius=' + radMetter +'&key=' + 'place api key here'
    
-      <Text>Home Screen </Text>
-      <Text>Hello {global.username}</Text>
-      <Button
-        title="Ask Someone"
-        onPress={() => navigation.navigate('Chatbot')}
-      />
+      fetch(url)
+       .then(resp => resp.json())
+        .then(res => {
+          console.log("unfiltered response")
+          // console.log(res)
+          var places = [] // This Array WIll contain locations received from google
+          for (let googlePlace of res.results) {
+            var place = {}
+            var lat = googlePlace.geometry.location.lat;
+            var lng = googlePlace.geometry.location.lng;
+            var coordinate = {
+              latitude: lat,
+              longitude: lng,
+            }
+  
+            var gallery = []
+   
+  
+            place['placeTypes'] = googlePlace.types
+            place['coordinate'] = coordinate
+            place['placeId'] = googlePlace.place_id
+            place['placeName'] = googlePlace.name
+            // place['gallery'] = gallery
+            place['neighborhood']=googlePlace.plus_code?googlePlace.plus_code.compound_code:0
+            if(place['neighborhood']!==0){
+              place['neighborhood']=place['neighborhood'].split(" ")
+              place['neighborhood'].splice(0,1)
+              place['neighborhood']=place['neighborhood'].join("")
+              place['neighborhood']=place['neighborhood'].split(",")[0]
+            }
+            
+          //  console.log("--------------------------------------")
+            console.log(place['neighborhood'])
+            places.push(place);
+          }
+          // console.log('places:')
+          // console.log(places)
+          // console.log(places.length)
+          // console.log(places[1].placeName)
+          // global.placeName=places[1].placeName
+          //Do your work here with places Array
+        })
+        .catch(error => {
+          console.log("error:")
+          console.log(error);
+        });
+  
+    })();
+
+ 
+  },[]);
+
+  
+  return (
+    // <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+    //   <Text>Home Screen {global.placeName}</Text>
+    //   <Text>Hello {global.username}</Text>
+    //   <Button
+    //     title="Ask Someone"
+    //     onPress={() => navigation.navigate('Chatbot')}
+    //   />
 
 
+    // </View>
+
+    <View style={styles.container}>
+      <MapView style={styles.mapStyle} region={{
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.04,
+        longitudeDelta: 0.005,
+      }} />
     </View>
+
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
 
