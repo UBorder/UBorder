2dc0303e2e592bdb34df4a84c220fc9a1d44aa55
AbdulmajeedSM <m.almqbl@gmail.com>
2020-04-29 22:21:59 +0300

    Ar Chatbot

--- Components/Selectlanguage.js
+++ Components/Selectlanguage.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React,{Component} from 'react'
+import { View, StyleSheet,Button ,Text, Image,TouchableOpacity } from 'react-native'
 
-import { View, StyleSheet, Text, Image,TouchableOpacity } from 'react-native'
-
-export default class Selectlanguage extends React.Component {
+ class Selectlanguage extends Component {
 
 
 render(){
     return (
-                <View>
-                    <TouchableOpacity>
-                        <Image source={require('../assets/saudi.png')} />
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                        <Image />
-                    </TouchableOpacity>
+                <View style={styles.button}>
+                    <Button  onPress={()=>{this.props.navigation.navigate("Meznah")}} title="English " />
+                    <View style={styles.button1}></View>
+                    <Button onPress={()=>{this.props.navigation.navigate("ArMeznah")}} title="Arabic" />
                 </View>
 
+
+
     );
+    
 }
 
 }
+const styles = StyleSheet.create({
+    button:{
+        marginTop: "20%",
+        justifyContent:"center"
+    },
+    button1:{
+        marginTop: "10%",
+        justifyContent:"center"
+    }
+})
+  
+export default Selectlanguage;
